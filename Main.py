6ba74e19a45ba1561a5e193d4e2ac5717298b972
Mateusz Jendernal <matij99@gmail.com>
2020-12-21 18:16:30 +0100

    added graph visualization method

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 from node import Node
 from graph import Graph
 from data import *
@@ -8,15 +12,14 @@ def getCPM(graph):
         if not node.kids:
             path = []
             paths = []
-            #print("Node " + str(node.name) + " has no kids")
-            dict.update(getCPMlastNode(node,path,paths))
+            dict.update(getCPMNode(node,path,paths))
     while len(dict)>1:    
         del dict[min(dict)]
     return dict
 
 
 
-def getCPMlastNode(node, path, paths):
+def getCPMNode(node, path, paths):
     if path:
         correctPath(node,path)
     else:
@@ -28,8 +31,7 @@ def getCPMlastNode(node, path, paths):
     else:
         out = []
         for parent in node.parents:
-            #print("WYpisuje: " + str(getCPMlastNode(parent, path,paths)))
-            key = next(iter(getCPMlastNode(parent, path,paths)))
+            key = next(iter(getCPMNode(parent, path,paths)))
             out.append(node.time + key)
     
 
@@ -45,9 +47,14 @@ def correctPath(node, path):
         return correctPath(node,path)
 
 graph = Graph(list)
+dict = getCPM(graph)
+#dict = getCPMNode(z10,[],[])
 
-for key, value in getCPM(graph).items():
+
+for key, value in dict.items():
     print(key)
     for i in value:
         for node in i: 
             print(node.name, end=" ")
+
+graph.show()            
--- graph.py
+++ graph.py
@@ -1,4 +1,32 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 class Graph:
     def __init__(self,listOfNodes):
         self.listOfNodes = listOfNodes
+
+    def show(self):
+        frm = []
+        to = []
+        '''for node in self.listOfNodes:
+            kids = []
+            frm.append(node.name)
+            for kid in node.kids:
+                kids.append(kid)
+            to.append(kids)'''
+        for node in self.listOfNodes:
+            for kid in node.kids:
+                frm.append(node.name)
+                to.append(kid.name)
+        
+
+        df = pd.DataFrame({ 'from':frm, 'to':to})
+        G=nx.from_pandas_edgelist(df,'from','to', create_using=nx.DiGraph())
+        nx.draw(G, with_labels=True, node_size=1500, alpha=0.3, arrows=True)
+        #&nbsp;
+        plt.show()
+    
 
